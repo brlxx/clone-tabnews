f517055468ed9387a971a1703d439859eb7fc8d4
Brlxx <brunoluan123luan@gmail.com>
2025-12-11 23:19:56 -0300

    Adiciona os scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 O TabNews é de certa forma interessante, juntamente com o curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Anonymous, se você estiver vendo isso me ligue agora mesmo no discord e imite um elefante!!</h1>;
+function Home() {
+  return (
+    <h1>
+      Anonymous, se você estiver vendo isso me ligue agora mesmo no discord e
+      imite um elefante!!
+    </h1>
+  );
 }
 
 export default Home;
